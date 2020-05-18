b94539310dfdec418dae33437d04ef4651227c43
Augusto-Gyuuki <59775769+Augusto-Gyuuki@users.noreply.github.com>
2020-05-18 16:15:32 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,43 +1,75 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fileIcon from './assets/file.png'
+import plusIcon from './assets/plus.png'
+import instructionsFile from './assets/Screenshot from 2020-05-18 14-55-05.png'
 import Axios from 'axios';
+import { useEffect } from 'react';
 
-function App() {
-  const [file, setFile] = useState()
-  const [relatorio, setRelatorio] = useState()
-  const [loading, setLoading] = useState()
-  
-  const handleSubmit = async() => {
-    setRelatorio(null)
-    setLoading(true)
-    await Axios.get('https://cvm-project.herokuapp.com/cdi')
-    const info  = new FormData()
-    info.append('CNPJ',file)
-    const { data } = await Axios.post('https://cvm-project.herokuapp.com/data', info)
-    setRelatorio(data)
-    setLoading(false)
-  }
-  let haveRelat = null 
-  if(relatorio){
-    haveRelat = <a href={relatorio}>Relatorio</a>
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <input type="file" onChange={async event => {
-            setFile(event.target.files[0])
-          }}/>
-          {loading ? null :<button onClick={() => {
-            handleSubmit()
-          }}>gerar relatorio</button>}
-        </p>
-        {loading ? <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>  : haveRelat}
-      </header>
-    </div>
-  );
+const App = () => {
+    const [file, setFile] = useState()
+    const [relatorio, setRelatorio] = useState()
+    const [pageLoading, setPageLoading] = useState(true)
+    const [loading, setLoading] = useState()
+
+    const handleSubmit = async() => {
+        setRelatorio(null)
+        setLoading(true)
+
+        const info  = new FormData()
+        info.append('CNPJ',file)
+        const { data } = await Axios.post('https://cvm-project.herokuapp.com/data', info)
+        
+        setRelatorio(data)
+        setLoading(false)
+    }
+    useEffect(async() => {
+        await Axios.get('https://cvm-project.herokuapp.com/cdi')
+        setPageLoading(false)
+    },[])
+    const spinner = <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    const loadingPage = <div class="lds-facebook"><div></div><div></div><div></div></div>
+    
+    let haveRelat = null 
+    if(relatorio){
+        haveRelat = <button className='input-button'>
+            <a href={relatorio} style={{ textDecoration: 'none', color: 'black'  }}>Relatorio</a>
+        </button>
+    }
+
+    return(
+        <>
+            {pageLoading ? <div style={{display: 'flex', height: "100vh", justifyContent: 'center', alignItems: 'center', background: '#000'}}>
+                {loadingPage}
+            </div> : null}
+            <div className={pageLoading ? 'loading' :"container"}>
+                <div className="header">
+                    <div className="title">CVM Project</div>
+                </div>
+                <div className="box">
+                    <div className="left">
+                        <div className="instructions">
+                            Adicione um arquivo .csv ou .tsv com o seguinte formato: <br/><br/>
+                            <img src={instructionsFile} className='instructions-img' alt="instruções do arquivo"/>
+                        </div>
+                    </div>
+                    <div className="input">
+                        <label className='file'>
+                            <input className='file-input' type="file" onChange={ event => {setFile(event.target.files[0])}}/>
+                            <img className='file-icon' src={file ? fileIcon : plusIcon} alt=""/>
+                        </label>
+                        {loading ? null : <button className='input-button' disabled={file ? false : true} onClick={() => { handleSubmit() }}>{file ? 'Gerar Relatorio' : 'Insira um Arquivo'}</button>}
+                        {loading ? spinner  : haveRelat}
+                    </div>
+                    <div className="right">
+                        No maximo arquivos com até 100 CNPJs <br/><br/> Tempo de espera : ~ 1 minuto
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
+
+
 
 export default App;
